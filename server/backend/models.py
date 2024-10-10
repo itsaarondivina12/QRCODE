aa6873b503536658085fdd2d31a1ffcd457cc12c
Aaron <aaron.divina@vxi.com>
2024-10-11 07:08:44 +0800

    Added Models

--- server/backend/models.py
+++ server/backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Registered_Users(models.Model):
+    Hrid = models.CharField(max_length=100, null=False)
+    FullName = models.CharField(max_length=100, null=False)
+    LocationDesc = models.CharField(max_length=100, null=False)
+    Team = models.CharField(max_length=100, null=False)
+    Test = models.CharField(max_length=100, default="DefaultTestValue", null=False)
+    DateRegistered = models.DateTimeField(null=False)
+
+    def __str__(self):
+        return self.FullName
+
